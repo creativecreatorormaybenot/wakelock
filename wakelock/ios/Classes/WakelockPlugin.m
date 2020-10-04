115a514f2cd5f5dce3b20089250502f324484933
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-10-04 13:38:26 +0000

    iOS implementation

--- wakelock/ios/Classes/WakelockPlugin.m
+++ wakelock/ios/Classes/WakelockPlugin.m
@@ -1,20 +1,31 @@
 #import "WakelockPlugin.h"
+#import "messages.h"
+
+@interface WakelockPlugin () <FLTWakelockApi>
+@end
 
 @implementation WakelockPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"wakelock"
-            binaryMessenger:[registrar messenger]];
   WakelockPlugin* instance = [[WakelockPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  FLTWakelockApiSetup(registrar.messenger, instance);
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
+- (void)toggle:(FLTToggleMessage*)input error:(FlutterError**)error {
+  NSNumber *enable = input.enable;
+  NSNumber *enabled = [NSNumber numberWithBool:[[UIApplication sharedApplication] isIdleTimerDisabled]];
+  
+  if (![enable isEqualToNumber:enabled]) {
+    [[UIApplication sharedApplication] setIdleTimerDisabled:enable.boolValue];
   }
+}
+
+- (FLTIsEnabledMessage*)isEnabled:(FlutterError* __autoreleasing *)error {
+  NSNumber *enabled = [NSNumber numberWithBool:[[UIApplication sharedApplication] isIdleTimerDisabled]];
+  
+  FLTIsEnabledMessage* result = [[FLTIsEnabledMessage alloc] init];
+  result.enabled = enabled;
+  
+  return result;
 }
 
 @end
