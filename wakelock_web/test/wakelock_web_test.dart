aeb5ddbd58dcf31a14517bdb635edcacfa45a888
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-10-04 16:12:33 +0000

    Web tests

--- wakelock_web/test/wakelock_web_test.dart
+++ wakelock_web/test/wakelock_web_test.dart
@@ -1,3 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wakelock/wakelock.dart';
+import 'package:wakelock_platform_interface/wakelock_platform_interface.dart';
+import 'package:wakelock_web/wakelock_web.dart';
+
+@TestOn('browser')
 void main() {
-  // todo: add tests. See https://github.com/flutter/plugins/blob/master/packages/video_player/video_player_web/test/video_player_web_test.dart.
+  group('$WakelockWeb', () {
+    setUpAll(() async {
+      WakelockPlatformInterface.instance = WakelockWeb();
+    });
+
+    test('$WakelockWeb set as default instance', () {
+      expect(WakelockPlatformInterface.instance, isA<WakelockWeb>());
+    });
+
+    test('initially disabled', () async {
+      expect(Wakelock.enabled, completion(isFalse));
+    });
+
+    test('enable', () async {
+      await Wakelock.enable();
+      expect(Wakelock.enabled, completion(isTrue));
+    });
+
+    test('disable', () async {
+      await Wakelock.disable();
+      expect(Wakelock.enabled, completion(isFalse));
+    });
+
+    test('toggle', () async {
+      await Wakelock.toggle(enable: true);
+      expect(Wakelock.enabled, completion(isTrue));
+
+      await Wakelock.toggle(enable: false);
+      expect(Wakelock.enabled, completion(isFalse));
+    });
+  });
 }
