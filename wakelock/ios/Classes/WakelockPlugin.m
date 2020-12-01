0834f8034b33acc44772d4babc31329c3774a1b9
LiYang <1353137283@qq.com>
2020-12-02 07:02:41 +0800

    fix: Avoid changes in values unexpected (#64)
    * fix: Avoid changes in values unexpected
    
    * Do not check stable
    
    * Attempt to fix integration test check
    
    * Add version
    
    * Remove extra pub get
    
    Co-authored-by: 李阳 <yang.li@plaso.me>
    Co-authored-by: creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>

--- wakelock/ios/Classes/WakelockPlugin.m
+++ wakelock/ios/Classes/WakelockPlugin.m
@@ -1,17 +1,25 @@
 #import "WakelockPlugin.h"
 #import "messages.h"
 
+static void * mKeyPathObserverContextApplicationIsIdleTimerDisabled = &mKeyPathObserverContextApplicationIsIdleTimerDisabled;
+
 @interface WakelockPlugin () <FLTWakelockApi>
+
+@property (nonatomic, assign) BOOL enable;
+
 @end
 
 @implementation WakelockPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   WakelockPlugin* instance = [[WakelockPlugin alloc] init];
   FLTWakelockApiSetup(registrar.messenger, instance);
+    
+  [UIApplication.sharedApplication addObserver:instance forKeyPath:@"idleTimerDisabled" options:NSKeyValueObservingOptionNew context:mKeyPathObserverContextApplicationIsIdleTimerDisabled];
 }
 
 - (void)toggle:(FLTToggleMessage*)input error:(FlutterError**)error {
   NSNumber *enable = input.enable;
+  self.enable = enable.boolValue;
   NSNumber *enabled = [NSNumber numberWithBool:[[UIApplication sharedApplication] isIdleTimerDisabled]];
   
   if (![enable isEqualToNumber:enabled]) {
@@ -26,6 +34,20 @@
   result.enabled = enabled;
   
   return result;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+  if (context == mKeyPathObserverContextApplicationIsIdleTimerDisabled) {
+    if (UIApplication.sharedApplication.idleTimerDisabled != self.enable) {
+      UIApplication.sharedApplication.idleTimerDisabled = self.enable;
+    }
+  } else {
+    [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+  }
+}
+
+- (void)dealloc {
+  [UIApplication.sharedApplication removeObserver:self forKeyPath:@"idleTimerDisabled" context:mKeyPathObserverContextApplicationIsIdleTimerDisabled];
 }
 
 @end
