1e62819ee7367482ce31c9191cb748a869c3c786
Wilson Wilson <hello@wilsonwilson.dev>
2021-01-17 00:48:35 +0100

    Add macOS implementation (#86)

---
+++ wakelock_macos/macos/Classes/WakelockMacosPlugin.swift
@@ -0,0 +1,47 @@
+import Cocoa
+import FlutterMacOS
+import IOKit.pwr_mgt
+
+public class WakelockMacosPlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "wakelock_macos", binaryMessenger: registrar.messenger)
+    let instance = WakelockMacosPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
+  }
+  
+  var assertionID: IOPMAssertionID = 0
+  var wakelockEnabled = false
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "toggle":
+    let args = call.arguments as? Dictionary<String, Any>
+    let enable = args!["enable"] as! Bool
+    if(enable){
+      enableWakelock()
+    }else {
+      disableWakelock();
+    }
+    result(true)
+    case "enabled":
+      result(wakelockEnabled)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+
+  func enableWakelock(reason: String = "Disabling Screen Sleep") {
+    if(!wakelockEnabled){
+      wakelockEnabled = IOPMAssertionCreateWithName( kIOPMAssertionTypeNoDisplaySleep as CFString,
+                                       IOPMAssertionLevel(kIOPMAssertionLevelOn),
+                                       reason as CFString,
+                                       &assertionID) == kIOReturnSuccess
+    }
+  }
+
+func disableWakelock() {
+    if wakelockEnabled {
+        IOPMAssertionRelease(assertionID)
+        wakelockEnabled = false
+    }
+  }
+}
