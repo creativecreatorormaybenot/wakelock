3d8cdf676e6c80fd03a4d0b8c64f8ebd404565e0
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-05-14 18:13:13 +0000

    Add empty test

--- wakelock/test/wakelock_test.dart
+++ wakelock/test/wakelock_test.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
   // There are no unit tests here as the API in this package only forwards the
   // calls to the platform interface (unit tested) → platform implementations.
   // Instead, this is all tested via e2e tests in the integration tests in the
   // example app.
+  test('no unit tests', () {});
 }
